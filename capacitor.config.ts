3b2cfac6f2c647d4a761983f5cd2cfd43f5aee4c
IvanUlloa098 <iulloa@est.up.edu.ec>
2021-12-25 12:23:31 -0500

    Creacion del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Application_3-1_practica',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
